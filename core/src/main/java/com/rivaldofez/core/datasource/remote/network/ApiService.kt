97a9e453274ebc5ddef48ddefbea24e318d6cd57
rivaldofez <rivaldofez@gmail.com>
2022-07-02 03:25:04 +0700

    add api service

--- core/src/main/java/com/rivaldofez/core/datasource/remote/network/ApiService.kt
+++ core/src/main/java/com/rivaldofez/core/datasource/remote/network/ApiService.kt
@@ -1,3 +1,24 @@
 package com.rivaldofez.core.datasource.remote.network
 
+import com.rivaldofez.core.datasource.remote.response.MovieDetailResponse
+import com.rivaldofez.core.datasource.remote.response.MoviesResponse
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface  ApiService {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") key: String,
+        @Query("page") page: String
+    ): MoviesResponse
+
+    @GET("movie/{id}")
+    suspend fun getDetailMovie(
+        @Path("id") id: String,
+        @Query("key") key : String
+    ): MovieDetailResponse?
+}
