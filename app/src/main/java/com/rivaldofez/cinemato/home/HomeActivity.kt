f70d6fb2f709c7c56226127a33756806dedec423
rivaldofez <rivaldofez@gmail.com>
2022-07-02 15:08:45 +0700

    add navigation fragment

--- app/src/main/java/com/rivaldofez/cinemato/home/HomeActivity.kt
+++ app/src/main/java/com/rivaldofez/cinemato/home/HomeActivity.kt
@@ -2,11 +2,17 @@ package com.rivaldofez.cinemato.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.rivaldofez.cinemato.R
+import com.rivaldofez.cinemato.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
