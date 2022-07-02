ed695cf942a5270240f6c3c01ce2d00299dca45c
rivaldofez <rivaldofez@gmail.com>
2022-07-02 19:24:10 +0700

    add action navigation controller

--- app/src/main/java/com/rivaldofez/cinemato/tvseries/TvShowFragment.kt
+++ app/src/main/java/com/rivaldofez/cinemato/tvseries/TvShowFragment.kt
@@ -35,7 +35,7 @@ class TVSeriesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tvseries, container, false)
+        return inflater.inflate(R.layout.fragment_tvshow, container, false)
     }
 
     companion object {
