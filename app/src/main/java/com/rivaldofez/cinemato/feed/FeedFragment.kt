406adbe05312ad955f0793bd1591474722336f0b
rivaldofez <rivaldofez@gmail.com>
2022-07-02 19:12:31 +0700

    add icon and setup navigation host in homeactivity

--- app/src/main/java/com/rivaldofez/cinemato/feed/FeedFragment.kt
+++ app/src/main/java/com/rivaldofez/cinemato/feed/FeedFragment.kt
@@ -1,5 +1,4 @@
-
-package com.rivaldofez.cinemato.movie
+package com.rivaldofez.cinemato.feed
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -15,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MovieFragment.newInstance] factory method to
+ * Use the [FeedFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MovieFragment : Fragment() {
+class FeedFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +35,7 @@ class MovieFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        return inflater.inflate(R.layout.fragment_feed, container, false)
     }
 
     companion object {
@@ -46,12 +45,12 @@ class MovieFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MovieFragment.
+         * @return A new instance of fragment FeedFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MovieFragment().apply {
+            FeedFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
