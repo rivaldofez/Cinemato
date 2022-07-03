f5a88bc62ae8f423b2b2e9265c593756a4d39b37
rivaldofez <rivaldofez@gmail.com>
2022-07-03 17:35:38 +0700

    implement data to recycler view on page movie fragment

--- app/src/main/java/com/rivaldofez/cinemato/movie/MovieFragment.kt
+++ app/src/main/java/com/rivaldofez/cinemato/movie/MovieFragment.kt
@@ -2,16 +2,22 @@
 package com.rivaldofez.cinemato.movie
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rivaldofez.cinemato.R
 import com.rivaldofez.cinemato.databinding.FragmentMovieBinding
+import com.rivaldofez.core.datasource.Resource
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MovieFragment : Fragment() {
     private var _binding: FragmentMovieBinding? = null
     private val binding get() = _binding!!
+    private val movieViewModel: MovieViewModel by viewModel()
+    private val popularAdapter = MovieAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +28,30 @@ class MovieFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(activity != null){
+            callObservePopularMovies()
+
+            with(binding.rvPopularMovie){
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+                adapter = popularAdapter
+            }
+        }
+    }
+
+    private fun callObservePopularMovies(){
+        movieViewModel.getPopularMovies("1").observe(viewLifecycleOwner, { movies ->
+            if(movies != null){
+                when(movies){
+                    is Resource.Success -> movies.data?.let { popularAdapter.setMovie(movies = it) }
+                    is Resource.Loading -> Log.d("Teston", "loading")
+                    is Resource.Error -> Log.d("Teston", "error")
+                }
+            }
+        })
+    }
 
 }
