2b838862e37b195e7e7f4d002c24f410a028ca15
rivaldofez <rivaldofez@gmail.com>
2022-07-04 01:53:28 +0700

    implement insert detail movie and tvshow in usecase interactor

--- core/src/main/java/com/rivaldofez/core/domain/usecase/CinemaInteractor.kt
+++ core/src/main/java/com/rivaldofez/core/domain/usecase/CinemaInteractor.kt
@@ -17,7 +17,7 @@ class CinemaInteractor(private val cinemaRepository: ICinemaRepository): CinemaU
 
     override fun getNowPlayingMovies(page: String): Flow<Resource<List<Movie>>> = cinemaRepository.getNowPlayingMovies(page)
 
-    override fun getDetailMovie(id: String): Flow<Resource<MovieDetail?>> = cinemaRepository.getDetailMovie(id = id)
+    override fun getDetailMovie(id: String): Flow<Resource<MovieDetail?>> = cinemaRepository.getDetailMovie(id)
 
     override fun getPopularTvShow(page: String): Flow<Resource<List<TvShow>>> = cinemaRepository.getPopularTvShow(page)
 
