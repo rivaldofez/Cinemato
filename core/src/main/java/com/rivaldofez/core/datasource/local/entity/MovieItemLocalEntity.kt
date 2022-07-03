d193ed56cad12606b20a51233daa35e178405614
rivaldofez <rivaldofez@gmail.com>
2022-07-03 15:40:18 +0700

    update movie field model

--- core/src/main/java/com/rivaldofez/core/datasource/local/entity/MovieItemLocalEntity.kt
+++ core/src/main/java/com/rivaldofez/core/datasource/local/entity/MovieItemLocalEntity.kt
@@ -12,43 +12,40 @@ data class MovieItemLocalEntity (
     @ColumnInfo(name = "id")
     val id: Int,
 
-//    @ColumnInfo(name = "overview")
-//    val overview: String,
-//
-//    @ColumnInfo(name = "original_language")
-//    val originalLanguage: String,
-//
-//    @ColumnInfo(name = "original_title")
-//    val originalTitle: String,
-//
-//    @ColumnInfo(name = "video")
-//    val video: Boolean,
-//
+    @ColumnInfo(name = "overview")
+    val overview: String,
+
+    @ColumnInfo(name = "original_language")
+    val originalLanguage: String,
+
+    @ColumnInfo(name = "original_title")
+    val originalTitle: String,
+
+    @ColumnInfo(name = "video")
+    val video: Boolean,
+
     @ColumnInfo(name = "title")
     val title: String,
-//
-////    @ColumnInfo(name = "genre_ids")
-////    val genreIds: List<Int>,
-//
-//    @ColumnInfo(name = "poster_path")
-//    val posterPath: String,
-//
-//    @ColumnInfo(name = "backdrop_path")
-//    val backdropPath: String,
-//
-//    @ColumnInfo(name = "release_date")
-//    val releaseDate: String,
-//
-//    @ColumnInfo(name = "popularity")
-//    val popularity: Double,
-//
-//    @ColumnInfo(name = "vote_average")
-//    val voteAverage: Double,
-//
-//    @ColumnInfo(name = "adult")
-//    val adult: Boolean,
-//
-//    @ColumnInfo(name = "vote_count")
-//    val voteCount: Int
+
+    @ColumnInfo(name = "poster_path")
+    val posterPath: String,
+
+    @ColumnInfo(name = "backdrop_path")
+    val backdropPath: String,
+
+    @ColumnInfo(name = "release_date")
+    val releaseDate: String,
+
+    @ColumnInfo(name = "popularity")
+    val popularity: Double,
+
+    @ColumnInfo(name = "vote_average")
+    val voteAverage: Double,
+
+    @ColumnInfo(name = "adult")
+    val adult: Boolean,
+
+    @ColumnInfo(name = "vote_count")
+    val voteCount: Int
 
 )
