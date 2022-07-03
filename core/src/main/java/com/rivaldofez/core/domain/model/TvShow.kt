765c8c65219f692e9de6b2894de332419e507c24
rivaldofez <rivaldofez@gmail.com>
2022-07-03 14:36:16 +0700

    add domain model

--- core/src/main/java/com/rivaldofez/core/domain/model/TvShow.kt
+++ core/src/main/java/com/rivaldofez/core/domain/model/TvShow.kt
@@ -1,4 +1,15 @@
 package com.rivaldofez.core.domain.model
 
-class TvShow {
-}
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+
+data class TvShow (
+    val id: Int,
+    val name: String,
+    val posterPath: String,
+    val backdropPath: String,
+    val originalName: String,
+    val popularity: Double,
+    val voteAverage: Double,
+)
